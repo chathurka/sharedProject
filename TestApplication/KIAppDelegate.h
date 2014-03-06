cb30d24d01324bf6c800ce1a84e9b331f84b2a5f
Chathurka Roshan <chathurka@calcey.com>
2014-03-06 19:55:43 +0530

    Initial Commit

---
+++ TestApplication/KIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KIAppDelegate.h
+//  TestApplication
+//
+//  Created by Chathurka on 3/6/14.
+//  Copyright (c) 2014 Chathurka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestApplication/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestApplication
+//
+//  Created by Chathurka on 3/6/14.
+//  Copyright (c) 2014 Chathurka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KIAppDelegate class]));
+    }
+}
